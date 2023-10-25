3125c3dda4707e899a971c610da55cf4520cc040
John Guibas <john@succinct.xyz>
2023-10-25 12:40:07 -0700

    feat: new

--- contracts/script/NounsRaffle.s.sol
+++ contracts/script/NounsRaffle.s.sol
@@ -8,7 +8,7 @@ contract RequestUpdateScript is Script {
     function run() public {
         vm.startBroadcast();
         address gateway = 0xE304f6B116bE5e43424cEC36a5eFd0B642E0dC95;
-        bytes32 functionId = 0xa13b44dd3b5470ca1c35d08c43ec37b82115db91af58ed2a3548fc2275b5a9b2;
+        bytes32 functionId = 0xfbb1246ca21e36bdc216ab69cafc3a365a18c3c5e6e04a9b90840311bf8882e1;
         address lightclient = address(0);
         address owner = 0xDEd0000E32f8F40414d3ab3a830f735a3553E18e;
         uint256 payoutAmount = 0;
